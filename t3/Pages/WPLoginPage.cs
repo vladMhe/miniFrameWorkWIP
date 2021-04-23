359ea2f4b781411c3f494dd88eb4fdeea8c81a7a
VladMHE <mihesan.vlad@gmail.com>
2021-04-23 17:35:24 +0300

    WordPressApp

---
+++ t3/Pages/WPLoginPage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenQA.Selenium;
+
+namespace t3.Pages
+{
+    class WPLoginPage : DriverHelper
+    {
+        IWebElement locationsTitle => Driver.FindElement(By.XPath("//span[contains(text(), 'Welcome to the world’s most')]"));
+        IWebElement logInButton => Driver.FindElement(By.XPath("//a[@title=\"Log In\"]"));
+
+        public IWebElement LocationsTitle { get { return locationsTitle; } }
+        public IWebElement LogInButton { get { return logInButton; } }
+
+
+
+
+
+
+        public void LogInButtonSelect()
+        {
+            LogInButton.Click();
+        }
+
+    }
+}
---
+++ t3/Tests/WordPressTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using t3;
+using t3.Pages;
+
+namespace WordPressLogin 
+{ 
+    class WordPressTests
+    {
+        DriverHelper helper = new DriverHelper();
+        WPLoginPage wpPage = new WPLoginPage();
+
+        [SetUp]
+        public void Setup()
+        {
+            helper.InitBrowser("Chrome");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            helper.EndSession();
+        }
+
+        //Verifying All, Open and Coming Soon locations
+        [Test]
+        public void Tesst()
+        {
+            helper.NavigateTo("https://wordpress.com/");
+            wpPage.LogInButtonSelect();
+        }
+    }
+}
