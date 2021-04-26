74172ea482d440df9c3866131a118233ef4c3eef
VladMHE <mihesan.vlad@gmail.com>
2021-04-26 14:51:25 +0300

    Added WordPress Login Tests

--- t3/Tests/WordPressTests.cs
+++ t3/Tests/WordPressTests.cs
@@ -7,7 +7,7 @@ using t3.Pages;
 
 namespace WordPressLogin 
 { 
-    class WordPressTests
+    class WordPressLogin
     {
         DriverHelper helper = new DriverHelper();
         WPLoginPage wpPage = new WPLoginPage();
@@ -16,20 +16,57 @@ namespace WordPressLogin
         public void Setup()
         {
             helper.InitBrowser("Chrome");
+            helper.BrowserManage();
         }
-
         [TearDown]
         public void TearDown()
         {
             helper.EndSession();
         }
 
-        //Verifying All, Open and Coming Soon locations
+        /*Logging in using an invalid userName*/
         [Test]
-        public void Tesst()
+        public void InvalidUserNameLogin()
         {
             helper.NavigateTo("https://wordpress.com/");
-            wpPage.LogInButtonSelect();
+            Assert.AreEqual(true, wpPage.WelcomeTitle.Displayed);
+            wpPage.logIn("wpsvcg@gmail.com@", "auto123auto");
+            Assert.AreEqual(true, wpPage.UserNotExistMessage.Displayed);
         }
+
+        /*Logging in using an invalid password*/
+        [Test]
+        public void InvalidPasswordLogin()
+        {
+            helper.NavigateTo("https://wordpress.com/");
+            Assert.AreEqual(true, wpPage.WelcomeTitle.Displayed);
+            wpPage.logIn("wpsvcg@gmail.com", "auto123aut@");
+            Assert.AreEqual(true, wpPage.WrongPassword.Displayed);
+        }
+
+        /*Checking field validations*/
+        [Test]
+        public void EmptyFieldsValidation()
+        {
+            helper.NavigateTo("https://wordpress.com/");
+            Assert.AreEqual(true, wpPage.WelcomeTitle.Displayed);
+            wpPage.logIn("", "");
+            Assert.AreEqual(true, wpPage.UserEmptyField.Displayed);
+            wpPage.UserField.SendKeys("wpsvcg@gmail.com");
+            wpPage.ContinueButton.Click();
+            wpPage.LoginButton.Click();
+            Assert.AreEqual(true, wpPage.PasswordField.Displayed);
+        }
+
+        /*Logging in using successfully*/
+        [Test]
+        public void SuccesfulLogin()
+        {
+            helper.NavigateTo("https://wordpress.com/");
+            Assert.AreEqual(true, wpPage.WelcomeTitle.Displayed);
+            wpPage.logIn("wpsvcg@gmail.com", "auto123auto");
+            Assert.AreEqual(true, wpPage.MyHomeTitle.Displayed);
+        }
+
     }
 }
